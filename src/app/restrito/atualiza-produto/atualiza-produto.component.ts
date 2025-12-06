fbd014df78acc4b8e36925e543cc0eea19c3111b
airton07 <airtonbrito81@gmail.com>
2025-12-05 21:36:47 -0300

    vai

--- src/app/restrito/atualiza-produto/atualiza-produto.component.ts
+++ src/app/restrito/atualiza-produto/atualiza-produto.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Produto } from 'src/app/models/Produto.model'; 
-import { ProdutoService } from 'src/app/produto.service'; 
-import { ActivatedRoute, Router } from '@angular/router'; 
+import { Produto } from 'src/app/models/Produto.model';
+import { ProdutoService } from 'src/app/produto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-atualiza-produto',
-  templateUrl: './atualiza-produto.component.html',
+  templateUrl: './Atualiza-Produto.Component.html',
   styleUrls: ['./atualiza-produto.component.css']
 })
 export class AtualizaProdutoComponent implements OnInit{
@@ -16,23 +16,24 @@ export class AtualizaProdutoComponent implements OnInit{
 
   constructor(private _produtoService: ProdutoService,private _activatedRoute: ActivatedRoute, private _router:Router) {
     this._activatedRoute.params.subscribe(params => this.produtoId = params['id']);
-}
+  }
 
-  ngOnInit(): void{
+  ngOnInit():void{
     this.listarProduto();
   }
 
   listarProduto():void{
     this._produtoService.getProduto(this.produtoId)
-    .subscribe((res: any) => { console.log(res[0].produto); 
+    .subscribe((res:any) => { console.log(res[0].produto);
       this.produto = new
-Produto(res[0].id,res[0].produto,res[0].descricao,res[0].foto,res[0].preco);
+      Produto(res[0].id,res[0].produto,res[0].descricao,res[0].foto,res[0].preco);
     })
   }
 
   atualizar(id: number){
-    this._produtoService.atualizarProduto(id,this.produto).subscribe(produto => { this.produto = new Produto(0, "", "", "", 0) }, 
-    err => { console.log("erro ao atualizar")}
+    this._produtoService.atualizarProduto(id,this.produto).subscribe(
+      produto => { this.produto = new Produto(0, "", "", "", 0)},
+      err => { console.log("erro ao atualizar")}
     );
 
     this._router.navigate(["/restrito/lista"]);
